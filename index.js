3dd55c76478dee8a85596bd48358da6462c44308
Gergesgamal <gg367743@gmail.com>
2026-07-17 13:43:13 +0300

    setup env

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log("welcome to my first dev ops project");
+setTimeout(() => {
+  console.log("I'm Soooooo Happyyyyyyyyyy");
+}, 3000);
---
+++ test.js
@@ -0,0 +1,6 @@
+console.log("Testing my First DevOps Project");
+setTimeout(() => {
+  console.log("setTimeout works well");
+}, 3000);
+
+console.log("tests completed successfully");
