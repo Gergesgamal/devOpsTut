b98b37ffc360ba49785cb54ad58c506437df4d64
Gerges <gg367743@gmail.com>
2026-09-08 17:21:38 +0300

    init commit

--- index.js
+++
@@ -1,4 +0,0 @@
-console.log("welcome to my first dev ops project");
-setTimeout(() => {
-  console.log("I'm Soooooo Happyyyyyyyyyy");
-}, 3000);
--- test.js
+++
@@ -1,6 +0,0 @@
-console.log("Testing my First DevOps Project");
-setTimeout(() => {
-  console.log("setTimeout works well");
-}, 3000);
-
-console.log("tests completed successfully");
